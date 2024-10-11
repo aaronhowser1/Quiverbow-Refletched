d39c0be198014f3bae17023c1d57bc05f6a92c39
Aaron Howser <aaronhowser1@gmail.com>
2024-10-11 17:37:05 -0500

    Revert "trying to move the guide things to clientside but you CANT ADD FRESH ENTITY TO CLIENT LEVEL"
    This reverts commit a82414ed4527d9eff49f2fd6a4b4996b26848fdd.

--- src/main/kotlin/dev/aaronhowser/mods/quiverbowrefletched/item/weapon/EnderBowItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/quiverbowrefletched/item/weapon/EnderBowItem.kt
@@ -1,15 +1,16 @@
 package dev.aaronhowser.mods.quiverbowrefletched.item.weapon
 
 import dev.aaronhowser.mods.quiverbowrefletched.entity.EnderBowGuideProjectile
-import net.minecraft.client.multiplayer.ClientLevel
-import net.minecraft.network.chat.Component
+import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.entity.projectile.Projectile
 import net.minecraft.world.item.BowItem
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
 import net.minecraft.world.level.Level
 import net.neoforged.neoforge.common.Tags
+import net.neoforged.neoforge.event.EventHooks
 import java.util.function.Predicate
 
 class EnderBowItem : BowItem(
@@ -21,24 +22,35 @@ class EnderBowItem : BowItem(
         return Predicate { checkedStack -> checkedStack.`is`(Tags.Items.INGOTS_IRON) }
     }
 
-    override fun onUseTick(level: Level, livingEntity: LivingEntity, usedStack: ItemStack, remainingUseDuration: Int) {
-        super.onUseTick(level, livingEntity, usedStack, remainingUseDuration)
+    override fun onUseTick(level: Level, livingEntity: LivingEntity, stack: ItemStack, remainingUseDuration: Int) {
+        super.onUseTick(level, livingEntity, stack, remainingUseDuration)
 
-        if (level is ClientLevel) {
+        if (level is ServerLevel) {
 
             if (level.gameTime % 5 != 0L) return
 
-            val projectileStack = livingEntity.getProjectile(usedStack)
+            val projectileStack = livingEntity.getProjectile(stack)
             if (projectileStack.isEmpty) return
 
-            val i = this.getUseDuration(usedStack, livingEntity) - remainingUseDuration
+            var i = this.getUseDuration(stack, livingEntity) - remainingUseDuration
+            if (livingEntity is Player) {
+                i = EventHooks.onArrowLoose(stack, level, livingEntity, i, !projectileStack.isEmpty)
+            }
+            if (i < 0) return
+
             val powerForTime = getPowerForTime(i)
 
             if (powerForTime > 0.1) {
-                shootGuideProjectile(
+                shoot(
                     level,
                     livingEntity,
-                    powerForTime * 3f
+                    livingEntity.usedItemHand,
+                    stack,
+                    listOf(Items.STICK.defaultInstance),
+                    powerForTime * 3f,
+                    1f,
+                    powerForTime == 1f,
+                    null
                 )
             }
         }
@@ -50,21 +62,6 @@ class EnderBowItem : BowItem(
         }
 
         return super.createProjectile(level, shooter, weapon, ammo, isCrit)
-    }
-
-    private fun shootGuideProjectile(
-        level: ClientLevel,
-        shooter: LivingEntity,
-        velocity: Float
-    ) {
-        val inaccuracy = 1f
-        val angle = 0f
-
-        val projectile = EnderBowGuideProjectile(shooter)
-        this.shootProjectile(shooter, projectile, 0, velocity, inaccuracy, angle, null)
-        level.addFreshEntity(projectile)
-
-        shooter.sendSystemMessage(Component.literal("Projectile exists: ${projectile.isAlive}"))
     }
 
 }
