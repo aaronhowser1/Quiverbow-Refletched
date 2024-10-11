419e314865854d025f956fc41bf19f817a270fc0
Aaron Howser <aaronhowser1@gmail.com>
2024-10-11 16:00:30 -0500

    only render for owner

--- src/main/kotlin/dev/aaronhowser/mods/quiverbowrefletched/entity/render/EnderBowGuideProjectileRenderer.kt
+++ src/main/kotlin/dev/aaronhowser/mods/quiverbowrefletched/entity/render/EnderBowGuideProjectileRenderer.kt
@@ -1,6 +1,8 @@
 package dev.aaronhowser.mods.quiverbowrefletched.entity.render
 
 import dev.aaronhowser.mods.quiverbowrefletched.entity.EnderBowGuideProjectile
+import dev.aaronhowser.mods.quiverbowrefletched.util.ClientUtil
+import net.minecraft.client.renderer.culling.Frustum
 import net.minecraft.client.renderer.entity.ArrowRenderer
 import net.minecraft.client.renderer.entity.EntityRendererProvider
 import net.minecraft.resources.ResourceLocation
@@ -11,6 +13,13 @@ class EnderBowGuideProjectileRenderer(
 
     override fun getTextureLocation(p0: EnderBowGuideProjectile): ResourceLocation {
         return ResourceLocation.withDefaultNamespace("textures/entity/projectiles/spectral_arrow.png")
+    }
+
+    override fun shouldRender(livingEntity: EnderBowGuideProjectile, camera: Frustum, camX: Double, camY: Double, camZ: Double): Boolean {
+        val owner = livingEntity.owner
+        if (owner != ClientUtil.localPlayer) return false
+
+        return super.shouldRender(livingEntity, camera, camX, camY, camZ)
     }
 
 }
