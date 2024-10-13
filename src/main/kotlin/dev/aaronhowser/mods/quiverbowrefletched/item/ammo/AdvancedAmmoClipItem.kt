a81389e3df9972782537482b5881435fde19d5f9
Aaron Howser <aaronhowser1@gmail.com>
2024-10-12 19:13:18 -0500

    working on advanced clip

--- src/main/kotlin/dev/aaronhowser/mods/quiverbowrefletched/item/ammo/AdvancedAmmoClipItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/quiverbowrefletched/item/ammo/AdvancedAmmoClipItem.kt
@@ -1,14 +1,34 @@
 package dev.aaronhowser.mods.quiverbowrefletched.item.ammo
 
-import dev.aaronhowser.mods.quiverbowrefletched.item.base.BasicAmmoHoldingItem
+import dev.aaronhowser.mods.quiverbowrefletched.item.component.ItemStackListComponent
+import dev.aaronhowser.mods.quiverbowrefletched.registry.ModDataComponents
+import net.minecraft.tags.TagKey
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
+import net.neoforged.neoforge.common.Tags
 
 class AdvancedAmmoClipItem(
-    maxAmmo: Int,
-    barColor: Int
-) : BasicAmmoHoldingItem(maxAmmo, barColor) {
+    private val maxAmmo: Int,
+    private val barColor: Int,
+    private val allowedAmmoTag: TagKey<Item>,
+    properties: Properties = Properties()
+        .stacksTo(1)
+        .component(
+            ModDataComponents.ADVANCED_AMMO_COMPONENT.get(),
+            ItemStackListComponent(maxAmmo)
+        )
+) : Item(properties) {
 
     companion object {
-        val SEED = AdvancedAmmoClipItem(512, 0x00FF00) // TODO: Include the thing to remember the seeds
+        val SEED = AdvancedAmmoClipItem(512, 0x00FF00, Tags.Items.SEEDS)
+    }
+
+    override fun isBarVisible(stack: ItemStack): Boolean {
+        return true
+    }
+
+    override fun getBarColor(stack: ItemStack): Int {
+        return barColor
     }
 
 }
