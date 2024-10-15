f236d259c34149baf5bb0955990dc2d5e1dce64d
Aaron Howser <aaronhowser1@gmail.com>
2024-10-15 16:25:12 -0500

    FlintDuster

--- src/main/kotlin/dev/aaronhowser/mods/quiverbowrefletched/item/weapon/FlintDuster.kt
+++ src/main/kotlin/dev/aaronhowser/mods/quiverbowrefletched/item/weapon/FlintDuster.kt
@@ -2,13 +2,23 @@ package dev.aaronhowser.mods.quiverbowrefletched.item.weapon
 
 import dev.aaronhowser.mods.quiverbowrefletched.item.base.BasicAmmoHoldingItem
 import dev.aaronhowser.mods.quiverbowrefletched.registry.ModItems
+import net.minecraft.core.BlockPos
 import net.minecraft.sounds.SoundEvents
 import net.minecraft.sounds.SoundSource
+import net.minecraft.tags.BlockTags
+import net.minecraft.world.InteractionHand
+import net.minecraft.world.InteractionResultHolder
 import net.minecraft.world.entity.SlotAccess
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.inventory.ClickAction
 import net.minecraft.world.inventory.Slot
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.ClipContext
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.phys.HitResult
+import net.neoforged.neoforge.common.CommonHooks
+import net.neoforged.neoforge.common.Tags
 
 class FlintDuster : BasicAmmoHoldingItem(
     maxAmmo = 256,
@@ -29,13 +39,13 @@ class FlintDuster : BasicAmmoHoldingItem(
     ): Boolean {
         if (thisStack.count != 1) return false
         if (action != ClickAction.SECONDARY || !slot.allowModification(player)) return false
-        if (getAmmo(thisStack) >= maxAmmo) return false
+        if (getAmmoCount(thisStack) >= maxAmmo) return false
         if (otherStack.item != ModItems.BOX_FLINT_DUST.get()) return false
 
         //TODO: Make sure I can insert if I have <32 missing ammo
 
         val otherStackSize = otherStack.count
-        val amountNeededForFullAmmo = (maxAmmo - getAmmo(thisStack)) / AMMO_PER_FLINT_BOX
+        val amountNeededForFullAmmo = (maxAmmo - getAmmoCount(thisStack)) / AMMO_PER_FLINT_BOX
         val amountToInsert = minOf(amountNeededForFullAmmo, otherStackSize)
 
         modifyAmmoCount(
@@ -44,7 +54,6 @@ class FlintDuster : BasicAmmoHoldingItem(
         )
         otherStack.shrink(amountToInsert)
 
-        //TODO: Custom sound?
         player.level().playSound(
             null,
             player.blockPosition(),
@@ -53,6 +62,72 @@ class FlintDuster : BasicAmmoHoldingItem(
             1f,
             0.33f
         )
+
+        return true
+    }
+
+    override fun use(
+        level: Level,
+        player: Player,
+        usedHand: InteractionHand
+    ): InteractionResultHolder<ItemStack> {
+        val usedStack = player.getItemInHand(usedHand)
+
+        val blockLookedAt = level.clip(
+            ClipContext(
+                player.eyePosition,
+                player.eyePosition.add(
+                    player.calculateViewVector(player.xRot, player.yRot).normalize().scale(7.0)
+                ),
+                ClipContext.Block.OUTLINE,
+                ClipContext.Fluid.NONE,
+                player
+            )
+        )
+
+        if (blockLookedAt.type == HitResult.Type.MISS) return InteractionResultHolder.fail(usedStack)
+
+        return if (mineBlock(
+                level,
+                blockLookedAt.blockPos,
+                player,
+                usedStack
+            )
+        ) {
+            InteractionResultHolder.sidedSuccess(usedStack, level.isClientSide)
+        } else {
+            InteractionResultHolder.fail(usedStack)
+        }
+    }
+
+    //TODO: Beam projectile
+    private fun mineBlock(
+        level: Level,
+        blockPos: BlockPos,
+        player: Player,
+        tool: ItemStack
+    ): Boolean {
+        if (!level.mayInteract(player, blockPos)) return false
+
+        val blockState = level.getBlockState(blockPos)
+        if (blockState.`is`(BlockTags.NEEDS_DIAMOND_TOOL) || blockState.`is`(Tags.Blocks.ORES)) return false
+        if (blockState.block.defaultDestroyTime() > 40) return false
+        if (level.getBlockEntity(blockPos) != null) return false
+
+        if (!CommonHooks.canEntityDestroy(level, blockPos, player)) return false
+
+        if (!entityUse(player, tool)) return false
+
+        Block.dropResources(
+            blockState,
+            level,
+            blockPos,
+            null,
+            player,
+            tool
+        )
+
+        level.destroyBlock(blockPos, false, player)
 
         return true
     }
